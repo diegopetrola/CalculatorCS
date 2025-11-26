60fc07e72dddfd83a36624fc734db29adcdd06a8
Diego Leal <diegopetrola@gmail.com>
2025-11-26 01:45:06 -0300

    Rebranched to fork repo

---
+++ CalculatorCS/Program.cs
@@ -0,0 +1,155 @@
+﻿using CalculatorLibrary;
+using System.Text.RegularExpressions;
+
+namespace CalculatorProgram;
+
+class Program
+{
+    public Calculator calculator;
+    Program()
+    {
+        calculator = new();
+    }
+
+    void printHistory()
+    {
+        var history = calculator.GetHistory();
+        for (int i = 0; i < history.Count; i++)
+        {
+            Operation op = history.ElementAt(i);
+            // Not a big fan of this ngl
+            bool showN2 = Regex.IsMatch(op.Operand ?? "", "[Add|Subtract|Power|Multiply|Divide]");
+            Console.WriteLine($"[{i}]- {op.Num1} {op.Operand} {(showN2 ? op.Num2 : "")} = {op.Result}");
+        }
+
+        Console.WriteLine("\n");
+    }
+
+    bool ParseInput(out double value)
+    {
+        value = 0;
+        string input = Console.ReadLine() ?? "";
+        if (input.Length == 0) return false;
+        if (input[0] == 'h')
+        {
+            printHistory();
+            return false;
+        }
+        else if (input[0] == 'u')
+        {
+            if (int.TryParse(input.Substring(1, 1), out int index))
+            {
+                var history = calculator.GetHistory();
+                if (index < history.Count)
+                {
+                    value = calculator.GetHistory().ElementAt(index).Result;
+                    Console.WriteLine($"...used value {value} from history.");
+                    return true;
+                }
+                else return false;
+            }
+        }
+        else if (double.TryParse(input, out value))
+        {
+            return true;
+        }
+        else if (input[0] == 'q')
+        {
+            Environment.Exit(0);
+        }
+        return false;
+    }
+
+    public bool ParseOperation(out string operation)
+    {
+        operation = Console.ReadLine() ?? "";
+        if (operation.Length == 0) return false;
+
+        if (Regex.IsMatch(operation, "[a|s|m|d|p|r|c|i]"))
+        {
+            return true;
+        }
+        if (operation[0] == 'q') Environment.Exit(0);
+        if (operation[0] == 'h')
+            printHistory();
+
+
+        return false;
+    }
+
+    static public void Main(string[] args)
+    {
+        Program app = new();
+
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine($"Calculator has been used {app.calculator.getOpenedCount()} times");
+        Console.WriteLine("------------------------\n");
+
+        while (true)
+        {
+            double result = 0;
+            Operation operation = new();
+
+            // Ask the user to choose an operator.
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.WriteLine("\tp - Power");
+            Console.WriteLine("\tr - Square root");
+            Console.WriteLine("\tc - Cosine");
+            Console.WriteLine("\ti - Sine");
+            Console.WriteLine("\th - See history");
+            Console.WriteLine("\tq - Quit");
+            Console.Write("Your option? ");
+            string op = "?";
+            while (!app.ParseOperation(out op))
+            {
+                Console.Write("Please choose an operation: ");
+            }
+
+            // Ask the user to type the first number.
+            Console.Write("Type a number, type 'h' to see history, type uN tu use index N of history: ");
+            double cleanNum1 = 0;
+            while (!app.ParseInput(out cleanNum1))
+            {
+                Console.Write("Please enter an integer value: ");
+            }
+
+
+            double cleanNum2 = 0;
+            if (op != null && Regex.IsMatch(op, "[a|s|m|d|p]"))
+            {
+                // Ask the user to type the second number.
+                Console.Write("Type a number, type 'h' to see history, type uN tu use index N of history: ");
+                while (!app.ParseInput(out cleanNum2))
+                {
+                    Console.Write("Please enter a valid option (number, 'h' or 'uN'): ");
+                }
+            }
+
+            operation.Num1 = cleanNum1;
+            operation.Num2 = cleanNum2;
+            operation.Operand = op;
+
+            Console.WriteLine("\n------------------------");
+            try
+            {
+                result = app.calculator.DoOperation(operation);
+                if (double.IsNaN(result))
+                {
+                    Console.Write("This operation will result in a mathematical error.\n");
+                }
+                else Console.Write("Your result: {0:0.##}\n", result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+            }
+
+            Console.WriteLine("------------------------\n");
+            Console.WriteLine("\n"); // Friendly linespacing.
+        }
+    }
+}
--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -4,120 +4,118 @@
 // OK! Add extra calculations: Square Root, Taking the Power, 10x, Trigonometry functions.
 
 using System.Text.Json;
-using System.IO;
 
-namespace CalculatorLibrary
+namespace CalculatorLibrary;
+
+public class Calculator
 {
-    public class Calculator
+    const string logPath = "../../../calculatorlog.json";
+    private CalculatorLog log;
+
+    public Calculator()
     {
-        const string logPath = "../../../calculatorlog.json";
-        private CalculatorLog log;
-
-        public Calculator()
-        {
-            string jsonString = File.Exists(logPath) ? File.ReadAllText(logPath) : "{}";
-            log = JsonSerializer.Deserialize<CalculatorLog>(jsonString) ?? new CalculatorLog();
-            log.OpenedCount++;
-            WriteLog();
-        }
-
-        public int getOpenedCount()
-        {
-            return log.OpenedCount;
-        }
-
-        public List<Operation> GetHistory()
-        {
-            return log.History;
-        }
-
-        void WriteLog()
-        {
-            int len = log.History.Count;
-            // Only write the last 5 elements, both to avoid cluttering the screen and logs too large
-            if (len > 5)
-                log.History = log.History.GetRange(len - 5, 5);
-            File.WriteAllText(logPath, JsonSerializer.Serialize<CalculatorLog>(log));
-        }
-
-        public double DoOperation(Operation operand)
-        {
-            operand.Result = double.NaN;
-            log.History.Add(operand);
-
-            // Use a switch statement to do the math.
-            switch (operand.Operand)
-            {
-                case "a":
-                    operand.Result = operand.Num1 + operand.Num2;
-                    operand.Operand = "Add";
-                    break;
-                case "s":
-                    operand.Result = operand.Num1 - operand.Num2;
-                    operand.Operand = "Subtract";
-                    break;
-                case "m":
-                    operand.Result = operand.Num1 * operand.Num2;
-                    operand.Operand = "Multiply";
-                    break;
-                case "d":
-                    // Ask the user to enter a non-zero divisor.
-                    if (operand.Num2 != 0)
-                    {
-                        operand.Result = operand.Num1 / operand.Num2;
-                        operand.Operand = "Divide";
-                    }
-                    else
-                    {
-                        log.History.RemoveAt(log.History.Count - 1); // remove from History
-                    }
-                    break;
-                case "p":
-                    operand.Result = Math.Pow(operand.Num1, operand.Num2);
-                    if(double.IsNaN( operand.Result))
-                    {
-                        log.History.RemoveAt(log.History.Count - 1);
-                    }
-                    operand.Operand = "Power";
-                    break;
-                case "r":
-                    if (operand.Num1 > 0)
-                    {
-                        operand.Result = Math.Sqrt(operand.Num1);
-                        operand.Operand = "Square Root";
-                    }
-                    else
-                    {
-                        log.History.RemoveAt(log.History.Count - 1);
-                    }
-                    break;
-                case "c":
-                    operand.Result = Math.Cos(operand.Num1);
-                    operand.Operand = "Cosine";
-                    break;
-                case "i":
-                    operand.Result = Math.Sin(operand.Num1);
-                    operand.Operand = "Sine";
-                    break;
-                // Return text for an incorrect option entry.
-                default:
-                    throw new Exception("Invalid Operation.");
-            }
-            WriteLog();
-
-            return operand.Result;
-        }
+        string jsonString = File.Exists(logPath) ? File.ReadAllText(logPath) : "{}";
+        log = JsonSerializer.Deserialize<CalculatorLog>(jsonString) ?? new CalculatorLog();
+        log.OpenedCount++;
+        WriteLog();
     }
-    public class Operation
+
+    public int getOpenedCount()
     {
-        public double Num1 { get; set; }
-        public double Num2 { get; set; }
-        public string? Operand { get; set; }
-        public double Result { get; set; }
+        return log.OpenedCount;
     }
-    public class CalculatorLog
+
+    public List<Operation> GetHistory()
     {
-        public int OpenedCount { get; set; }
-        public List<Operation> History { get; set; } = [];
+        return log.History;
     }
+
+    void WriteLog()
+    {
+        int len = log.History.Count;
+        // Only write the last 5 elements, both to avoid cluttering the screen and logs too large
+        if (len > 5)
+            log.History = log.History.GetRange(len - 5, 5);
+        File.WriteAllText(logPath, JsonSerializer.Serialize<CalculatorLog>(log));
+    }
+
+    public double DoOperation(Operation operand)
+    {
+        operand.Result = double.NaN;
+        log.History.Add(operand);
+
+        // Use a switch statement to do the math.
+        switch (operand.Operand)
+        {
+            case "a":
+                operand.Result = operand.Num1 + operand.Num2;
+                operand.Operand = "Add";
+                break;
+            case "s":
+                operand.Result = operand.Num1 - operand.Num2;
+                operand.Operand = "Subtract";
+                break;
+            case "m":
+                operand.Result = operand.Num1 * operand.Num2;
+                operand.Operand = "Multiply";
+                break;
+            case "d":
+                // Ask the user to enter a non-zero divisor.
+                if (operand.Num2 != 0)
+                {
+                    operand.Result = operand.Num1 / operand.Num2;
+                    operand.Operand = "Divide";
+                }
+                else
+                {
+                    log.History.RemoveAt(log.History.Count - 1); // remove from History
+                }
+                break;
+            case "p":
+                operand.Result = Math.Pow(operand.Num1, operand.Num2);
+                if (double.IsNaN(operand.Result))
+                {
+                    log.History.RemoveAt(log.History.Count - 1);
+                }
+                operand.Operand = "Power";
+                break;
+            case "r":
+                if (operand.Num1 > 0)
+                {
+                    operand.Result = Math.Sqrt(operand.Num1);
+                    operand.Operand = "Square Root";
+                }
+                else
+                {
+                    log.History.RemoveAt(log.History.Count - 1);
+                }
+                break;
+            case "c":
+                operand.Result = Math.Cos(operand.Num1);
+                operand.Operand = "Cosine";
+                break;
+            case "i":
+                operand.Result = Math.Sin(operand.Num1);
+                operand.Operand = "Sine";
+                break;
+            // Return text for an incorrect option entry.
+            default:
+                throw new Exception("Invalid Operation.");
+        }
+        WriteLog();
+
+        return operand.Result;
+    }
+}
+public class Operation
+{
+    public double Num1 { get; set; }
+    public double Num2 { get; set; }
+    public string? Operand { get; set; }
+    public double Result { get; set; }
+}
+public class CalculatorLog
+{
+    public int OpenedCount { get; set; }
+    public List<Operation> History { get; set; } = [];
 }
